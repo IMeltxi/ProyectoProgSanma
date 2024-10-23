4a740c2ddb8b7d55cd1ae40f06880bc0d560b04d
Iñigo_Melchisidor <i.melchisidor@opendeusto.es>
2024-10-23 12:01:50 +0200

    Primer Commit

---
+++ Proyecto_Prog_Sanma/src/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		System.out.println("Adios");
+		// TODO Auto-generated method stub
+
+	}
+
+}
