87eac620e03f8b65b15d5e6ec31268119d692b8c
Ander González <ander.gonzalez.a@opendeusto.es>
2024-10-23 12:07:17 +0200

    prueba ander

--- Proyecto_Prog_Sanma/src/Principal.java
+++ Proyecto_Prog_Sanma/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 		System.out.println("Adios");
+		System.out.println("Comit ANder");
 		// TODO Auto-generated method stub
 
 	}
