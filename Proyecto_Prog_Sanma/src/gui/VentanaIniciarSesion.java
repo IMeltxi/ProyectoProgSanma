85e7e7a9ad7ee92fe0d5f86c245000741b1abc58
Ander González <ander.gonzalez.a@opendeusto.es>
2024-10-27 17:00:58 +0100

    Centrado del contenido y titulo del contenido agregado

--- Proyecto_Prog_Sanma/src/gui/VentanaIniciarSesion.java
+++ Proyecto_Prog_Sanma/src/gui/VentanaIniciarSesion.java
@@ -1,12 +1,17 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -15,6 +20,9 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 public class VentanaIniciarSesion extends JFrame {
 	
@@ -27,11 +35,17 @@ public class VentanaIniciarSesion extends JFrame {
 		setLocationRelativeTo(null);
 		
 		
-		//Panel principal
-		JPanel panelprincipal = new JPanel(new BorderLayout());
-		
+		 // Panel principal con GridBagLayout para centrar
+        JPanel panelprincipal = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        
 		//BoxLayaout
 		JPanel panel2 = new JPanel();
+		TitledBorder titledBorder = BorderFactory.createTitledBorder("Inicio de Sesion");
+        panel2.setBorder(BorderFactory.createCompoundBorder(titledBorder,new EmptyBorder(20, 20, 20, 20)));
 		panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
 		
 		//Correo etiqueta
@@ -76,6 +90,10 @@ public class VentanaIniciarSesion extends JFrame {
 				//condicion para iniciar sesion
 				
 				if(gmail.equals("ejemplo") && contraseña.equals("ejemplo")) {
+					/** codigo a poner:
+					 * 		if gmail in ListaSocios and contraseña = contraseña del mapa con clave gmail
+					 * */
+					
 					JOptionPane.showMessageDialog(null, "inicio valido");
 				}else {
 					JOptionPane.showMessageDialog(null, "inicio no valido");
@@ -84,13 +102,11 @@ public class VentanaIniciarSesion extends JFrame {
 			}
 		});
 		
-		//panel para el boton
-		JPanel panelboton =new JPanel();
-		panelboton.add(botonInicioSesion);
+		//Agregar boton de inicio de sesion al panel
+		panel2.add(botonInicioSesion);
 		
-		//paneles
-		panelprincipal.add(panel2, BorderLayout.CENTER);
-		panelprincipal.add(panelboton, BorderLayout.SOUTH);
+		// Agregar panel2 centrado en el panel principal
+        panelprincipal.add(panel2, gbc);
 		
 		add(panelprincipal);
 		
