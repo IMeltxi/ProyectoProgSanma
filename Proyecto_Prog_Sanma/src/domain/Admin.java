f16dbba625abf6f692d2a14a2e9302a3f022507d
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2024-11-04 13:20:10 +0100

    Continuacion clase admin

--- Proyecto_Prog_Sanma/src/domain/Admin.java
+++ Proyecto_Prog_Sanma/src/domain/Admin.java
@@ -2,9 +2,39 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import domain.Usuario.tipoSocio;
 
 public class Admin {
 
-		private ArrayList<Usuario>listaUsuaraios;
-		private ArrayList<Partido>listaPartidos;
+		private Map<tipoSocio,ArrayList<Usuario>>mapaUsuarios;
+		private List<Usuario>listaEsperaUsuarios;
+		private List<Partido>listaPartidos;
+		
+		public Admin() {
+			super();
+			mapaUsuarios = new HashMap<tipoSocio,ArrayList<Usuario>>();
+			listaEsperaUsuarios = new LinkedList<Usuario>();
+			listaPartidos = new ArrayList<Partido>();
+			
+		}
+		
+		
+		private void añadirUsuario(Usuario user) {
+			if(user.getTiposocio().equals(tipoSocio.GAZTEABONO)) {
+				if(!mapaUsuarios.containsKey(user.getTiposocio())) {
+					mapaUsuarios.put(user.getTiposocio(), new ArrayList<>());		
+				}
+				mapaUsuarios.get(user.getTiposocio()).add(user);
+			}
+		}
+		
+		private void borrarUsuario(Usuario user) {
+		}
+		
+		
+		
 }
