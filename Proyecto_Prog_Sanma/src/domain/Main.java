205817f346f75edccd55aa1a1af04b35daec81e9
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2024-11-17 16:07:09 +0100

    Intento de sacar lista usuarios por patalla.
    Revisar la fecha de nacimiento ya que creo que el error surge de ahi.

--- Proyecto_Prog_Sanma/src/domain/Main.java
+++ Proyecto_Prog_Sanma/src/domain/Main.java
@@ -1,7 +1,10 @@
 package domain;
 
+import domain.Admin;
 public class Main {
 	public static void main(String[] args) {
-		
+		Admin a = new Admin();
+		a.cargarUsuarios();
+		System.out.println(a.getUsuarios());
 	}
 }
