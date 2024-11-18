665901019d58282c01910853c27a12852162e427
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2024-11-18 12:38:28 +0100

    codigo a revisar, posiblemente codigo a borrar

--- Proyecto_Prog_Sanma/src/domain/Main.java
+++ Proyecto_Prog_Sanma/src/domain/Main.java
@@ -1,10 +1,13 @@
 package domain;
 
+import java.util.List;
+
 import domain.Admin;
 public class Main {
 	public static void main(String[] args) {
 		Admin a = new Admin();
 		a.cargarUsuarios();
-		System.out.println(a.getUsuarios());
+		
+		//List<Usuario> usuarios = a.cargarUsuarios(); Da error ya que el metodo no devuelve nada
 	}
 }
