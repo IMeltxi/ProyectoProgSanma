ed32781c5d8a6bc0ee3eed0e10a5441d9eefad90
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2025-01-15 21:07:15 +0100

    quitar funcion recursiva del main

--- Proyecto_Prog_Sanma/src/domain/Main.java
+++ Proyecto_Prog_Sanma/src/domain/Main.java
@@ -20,32 +20,7 @@ public class Main  {
 		//BD.crearTablas();
 		
 		pruebas();
-		
-		//Funcion recursiva
-		Partido[] partidos = {
-			    new Partido("Athletic vs Getafe CF","2024-08-12","21:30",1),
-			    new Partido("Athletic vs Valencia","2024-08-27","21:30",3               ),
-			    new Partido("Athletic vs Atletico de Madrid","2024-09-16","18:30",5     ),
-			    new Partido("Athletic vs RC Celta de Vigo","2024-09-27","19:00",7       ),
-			    new Partido("Athletic vs Sevilla FC","2024-10-08","16:15",9             ),
-			    new Partido("Athletic vs RCD Espanyol","2024-10-29","16:15",11          ),
-			    new Partido("Athletic vs Betis","2024-11-05","18:30",12                 ),
-			    new Partido("Athletic vs Real Sociedad","2024-12-03","21:00",15         ),
-			    new Partido("Athletic vs Real Madrid","2024-12-17","16:15",17           ),
-			    new Partido("Athletic vs Villareal","2025-01-07","16:15",19             ),
-			    new Partido("Athletic vs Leganés","2025-01-28","18:30",21               ),
-			    new Partido("Athletic vs Girona FC","2025-02-11","16:15",23             ),
-			    new Partido("Athletic vs Real Valladolid CF","2025-02-25","18:30",25    ),
-			    new Partido("Athletic vs RCD Mallorca","2025-03-10","16:15",27          ),
-			    new Partido("Athletic vs CA Osasuna","2025-03-31","18:30",29            ),
-			    new Partido("Athletic vs Rayo Vallecano","2025-04-14","16:15",31        ),
-			    new Partido("Athletic vs UD Las Palmas","2025-04-28","18:30",33         ),
-			    new Partido("Athletic vs Deportivo Alavés","2025-05-19","18:30",36      ),
-			    new Partido("Athletic vs FC Barcelona","2025-05-26","18:30",38          )
-			};
-
-		Partido masCercano = Admin.buscarPartidoMasCercanoRecursividad(partidos, 0, "2024-03-30");
-		System.out.println("Partido más cercano: " + masCercano);
+	
 	}
 	
 	public static void pruebas() {
